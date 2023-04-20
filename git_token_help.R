4e354ba371a9d22707215d219a799220e4043022
klangwig <klangwig@gmail.com>
2023-04-20 11:15:07 -0400

    git min lecture

---
+++ git_token_help.R
@@ -0,0 +1,25 @@
+#creating git credentials
+
+#Instructions from this website:
+
+#you will need to then follow these directions:
+#https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
+
+#1 Create your personal access token
+usethis::create_github_token()
+#Look over the scopes; I highly recommend selecting 
+#“repo”, “user”, and “workflow”. 
+#Click “Generate token”.
+
+#Copy the generated PAT to your clipboard! paste it someplace (notes, a word doc)
+#you will never be able to see it again so this is important
+#VERY IMPORTANT#
+#make sure you save your personal access token!!
+#if you navigate away from this, you will never see it again!
+
+#2. Run the following lines
+install.packages("gitcreds")
+library(gitcreds)
+gitcreds_set()
+#paste your personal access token
+gitcreds_get()
